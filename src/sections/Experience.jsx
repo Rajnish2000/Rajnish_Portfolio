d82de78b017aabb7f6da0ed1679953ca3529bfd3
rajnish2025 <rajnish@veriqos.com>
2025-12-08 11:46:06 +0530

    minor changes

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -38,7 +38,7 @@ const Experience = () => {
   }, []);
   return (
     <section
-      className="w-full md:mt-20 section-padding xl:px-0 relative"
+      className="w-full md:mt-40 mt-40 md:mb-20 section-padding xl:px-0 relative"
       id="experience"
     >
       <div className="w-full h-full md:px-20 px-5">
